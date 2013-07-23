e1bb053379c043fb8d45bfc3b6433cb1968a12da
dbc-apprentice <dbc@dbc.com>
2013-07-22 22:50:26 -0700

    added bootstrap and login menu

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,51 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
+  //This is to keep loginmodal window centered after adjusting width.
+  
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  
+  $('#login_li').on('click', function(){
+    $("#loginModal").modal('show');
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+$('#loginModal .modal-footer button').on('click', function(event){
+  event.preventDefault();
+  if (!validate_email($('#login [name="email"]').val())){
+    $('[for="email"]').append(render_error("email"))
+  }
+  $('#login input').focus(function(){
+    console.log(this)
+    $('[for="email"]').remove('span');
+  })
 });
+
+
+
+
+});
+
+
+function validate_email(email){
+  var emailRegex = new RegExp(/^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$/i);
+  return emailRegex.test(email);
+}
+
+function render_error(item){
+  return '<span style="color: red"> - ' + item + ' incorrect format</span>'
+}
+
+
+function modal_render(modal_id){
+  $( modal_id + ' button').attr("disabled","disabled");
+  $( modal_id + ' button').text('Fill in Form');
+}
